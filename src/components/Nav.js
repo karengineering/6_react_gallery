578179e3933a3c2dda9f51048fd4d477af9e7539
Karen Ho <49111213+karengineering@users.noreply.github.com>
2022-07-29 15:31:12 -0700

    fix ref

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,9 +8,9 @@ const Nav = () => (
         <ul>
             <li><NavLink to='/cats'>Cats</NavLink></li>
             <li><NavLink to='/dogs'>Dogs</NavLink></li>
-            <li><NavLink to='/computers'>Computers</NavLink></li>
+            <li><NavLink to='/birds'>Birds</NavLink></li>
         </ul>
     </nav>
 );
 
-export default Nav;
+export default Nav; 
